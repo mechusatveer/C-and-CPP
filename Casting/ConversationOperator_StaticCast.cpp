e894254348b3c4efb816025771aed54a56e0929e
Satveer Singh Mechu <mechu.satveer@gmail.com>
2014-12-21 03:30:10 +0530

    Update ConversationOperator_StaticCast.cpp

--- Casting/ConversationOperator_StaticCast.cpp
+++ Casting/ConversationOperator_StaticCast.cpp
@@ -17,20 +17,50 @@ conversation operator not have a return type
 
 operator op_name()
 {
-
+     return op_name(args);
 }
 class can have a public method for specific data type conversions.
 for example:
 class Boo
 {
-double value;
-public:
-Boo(int i )
-operator double()
-{
-return value;
-}
+  double value;
+  public:
+  Boo(int i )
+  operator double()
+  {
+      return value;
+  }
 };
 Boo BooObject;
 double i = BooObject; // assigning object to variable i of type double.
 now conversion operator gets called to assign the value. 
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class A
+{
+  int x;
+  public:
+  A(int i):x(i){}
+  void print()
+  {
+    cout<<x<<endl;
+  }
+};
+class B
+{
+   public:
+       operator A()
+       {
+           return A(3);
+       }
+};
+int main()
+{
+    B b;
+    A a = static_cast<A>(b);
+    a.print();
+    return 0;
+}
