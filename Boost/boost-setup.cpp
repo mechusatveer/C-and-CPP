994c657ca857d8085ca356b1aadc1ce6c7c4a320
Satveer Singh Mechu <mechu.satveer@gmail.com>
2015-01-16 12:11:18 +0530

    Create boost-setup.cpp

---
+++ Boost/boost-setup.cpp
@@ -0,0 +1,9 @@
+Download boost source code
+Extract it 
+Open command line and go to boost_1_54_0
+execute bootstrap
+
+Now u need to configure your studio for it
+
+add c:\boost_1_54_0 in your include path
+add c:\boost_1_54_0\stage\lib in your lib path
