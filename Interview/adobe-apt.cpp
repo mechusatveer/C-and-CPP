137741c96324f36347ee3cba20eaeee77af7c3f8
Satveer Singh Mechu <mechu.satveer@gmail.com>
2015-01-19 21:33:10 +0530

    Update adobe-apt.cpp

--- Interview/adobe-apt.cpp
+++ Interview/adobe-apt.cpp
@@ -1,1 +1,121 @@
-Aptitude Test
+
+=========================================================================================================================
+Question
+36 identical chairs must be arranged in rows with the same number of chairs in each row. 
+Each row must contain at least three chairs and there must be at least three rows. The
+row is parallel to the front of the room. How many different arrangements are possible? 
+Pick one of the choices
+a) 2 b) 4 c) 5 d)6 e) 10
+
+Answer C   5 different arrangement are possible
+=========================================================================================================================
+Question
+ABCD is a rectangle. The length of BE is 4 and the length of EC is 6. The area of the 
+triangle BEA plus the area of the triangle DCE minus the area of the triangle AFO is.
+a) 0 b) 0.4 of the area of the triangle AEB c)0.5 of the area of the triangle AED d)0.5 of the area of ABCD
+e)cannot be determined
+
+Answer a Diff of area be zero
+=========================================================================================================================
+Question
+In a group of people solicited by a charity, 30% contributed $40 each, 45% contributed $20 each and the 
+rest contributed $12 each. What is the percentage of the total contributed came from the people 
+who gave $40?
+a)25% b)30% c)40% d)45% e)50%
+
+Anwer e  it be 50% of total 
+=========================================================================================================================
+Question
+A pension fund has a total of $1 million invested in stock of the ABC company and the bonds of the DEE Corporation.
+The ABC stocks yields 12% in cash each year, and the DEE bonds pay 10% in cash each year. The pension fund 
+received a total of $115,000 in cash from ABC stock and the DEE bonds last year. How much money was invested 
+in ABC stock?
+
+a)$750,000 b)$600,000 c)$500,000 d)$333,333.33 e)$250,000
+Answer TODO
+=====================================================================================================================
+Question
+1)A cylinder of radius 5 mm and height 11 mm
+2)A sphere of radius 6mm
+3)A cube with edge 9mm
+
+Whose volume be higher
+Answer;- A sphere of 6mm radius will high volumn
+4/3PI R^3
+====================================================================================================================
+The ratio of the chickens to pigs to horses on a farm can be expressed as the triple ratIo 20:4:6
+If there are 120 chickens on the farm, then the number of horses on the farm Is :
+a)4  b)6   c)24   d)36   e)60
+
+Answer:- 36  d
+====================================================================================================================
+Question
+x2 — y2 =15, and x + y =3 then x — y is :
+-3  0   3   5   Cannot be determined
+
+Answer 5
+====================================================================================================================
+Question
+Two white O O inside big black O What is the area of the shaded region? The radius of the outer circle is a 
+and the radius of each of the circles inside the larger circle is a/3 :
+
+(1/3)na2  (2/3)na2   (7/9)na2   (8/9)na2
+
+Answer:-
+7/9 PI a^2
+===================================================================================================================
+Question
+he operation * applied to a number gives as its result 10 subtracted from twice the number. What is * (*9)?
+
+*x = 2x-10
+so *9 = 2*9-10 = 8
+again
+*8= 2*8-10 = 6
+
+Answer is 6
+====================================================================================================================
+Question
+If x/y = 3/z then 9y^2 equals
+
+Answer:- x2z2
+====================================================================================================================
+A car travels 75% of the way from town A to town B by travelling for T hours at an average speed of V mph.
+The car travels at an average speed of S mph for the remaining part of the trip. Which of the following
+expressions represents the time the car travelled at S mph?
+
+Answer was VT/3S
+===================================================================================================================
+If you rearrange the letters ITORSHC you’ll get the name of a famous. Pick one of the choices
+City
+Animal (Correct)
+Sportsperson
+Movie Star
+
+Answer:-Bird
+===================================================================================================================
+If MILK is written as 1391211 How would you write CURD Pick one of the choices
+311073
+3121184
+221194
+223173
+None of the above
+
+Answer:- None of above
+===================================================================================================================
+
+George adores classical music. He always prefers Beethoven to Bartok and Mahler to Mozart. 
+He prefers Haydn to Hindernith and Hlitdeiulth to Mozart. He always prefers Mahler to any 
+whose name begins with B, except Beethoven, and he always chooses to listen to a he prefers.
+
+Question 1:-
+George’s brother gives him one recording by each of the composers mentioned. Which of the 
+following correctly states the order in which George must play some of the records? 
+Pick one of the choices
+
+Beethoven, Bartok, Mozart
+Haydn, Hindemith, Mozart
+Beethoven, Mahler, Bartok
+Hindemith, Mahier, Mozart
+Haydn, Hindemith, Mahler
+
+Answer:-TODO
