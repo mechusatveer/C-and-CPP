2a953b917238f56e6ad919330ab6341b21d0e61e
Satveer Singh Mechu <mechu.satveer@gmail.com>
2015-03-16 18:30:02 +0530

    Update Edelweiss.cpp

--- Interview/Edelweiss.cpp
+++ Interview/Edelweiss.cpp
@@ -29,3 +29,11 @@ class MostDerived : public Derived
 
 if we call fun from destructor it be a issue
 
+It is usually not a good idea to call a virtual function from a constructor/destructor
+
+The reason for this is that dynamic dispatch is strange during these two operations. 
+The actual type of the object changes during construction and it changes again during
+destruction. When a destructor is being executed, the object is of exactly that type,
+and never a type derived from it. Dynamic dispatch is in effect at all time, but the
+final overrider of the virtual function will change depending where in the hierarchy
+you are.
